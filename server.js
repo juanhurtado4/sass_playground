d26ef642c5d0bbbd90af99aecaaad89963f96b95
juan hurtado <juanhurtado4@outlook.com>
2018-03-10 23:12:17 -0800

    Feat: Add dependecy for live reloading

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const reload = require('reload');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,6 +14,8 @@ app.use(express.static('img'));
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+reload(app);
 
 app.listen('3000', () => {
     console.log('Server running in port: http://localhost:3000/')
